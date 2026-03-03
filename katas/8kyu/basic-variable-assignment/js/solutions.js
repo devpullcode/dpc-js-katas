e749677f4fbf06b194e470f5e74414797c5d7e31
Jose Antonio González Santos <contact@devpullcode.dev>
2026-03-03 19:40:45 +0100

    feat(kata): ✨ implementa solución en JS y TS para basic-variable-assignment

---
+++ katas/8kyu/basic-variable-assignment/js/solutions.js
@@ -0,0 +1,12 @@
+/**
+ * 8kyu - Basic Variable Assignment
+ *
+ * Expected output: "codewa.rs"
+ */
+export function basic_variable_assignment() {
+  const a = 'code';
+  const b = 'wa.rs';
+  const name = a + b;
+
+  return name;
+}
---
+++ katas/8kyu/basic-variable-assignment/ts/solutions.ts
@@ -0,0 +1,12 @@
+/**
+ * 8kyu - Basic Variable Assignment
+ *
+ * Expected output: "codewa.rs"
+ */
+export function basic_variable_assignment(): string {
+  const a: string = 'code';
+  const b: string = 'wa.rs';
+  const name: string = a + b;
+
+  return name;
+}
